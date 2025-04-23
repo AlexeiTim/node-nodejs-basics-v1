4931a3472eee7f973733607fa2796dd28bc732f5
timashkov <timashkov060896@gmail.com>
2025-04-24 00:04:28 +0300

    feat: add create file realization

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
+
+const __dirname = import.meta.dirname
+const filePath = join(__dirname, 'files/fresh.txt')
+
+const FILE_CONTENT = 'I am fresh and young'
+const ERROR_MESSAGES = {
+    FS_OPERATION_FAILED: 'FS operation failed'
+}
+const ERROR_CODES = {
+    FILE_NOT_FOUND: 'ENOENT',
+}
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.access(filePath);
+        throw new Error(ERROR_MESSAGES.FS_OPERATION_FAILED)
+    } catch(e) {
+        if (e.code !== ERROR_CODES.FILE_NOT_FOUND)
+            throw e
+
+        await fs.writeFile(filePath, FILE_CONTENT);
+    }
 };
 
 await create();
