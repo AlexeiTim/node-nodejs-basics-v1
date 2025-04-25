3ed910fa1876eb3f9fe41886d362ddc02368643b
timashkov <timashkov060896@gmail.com>
2025-04-25 23:12:53 +0300

    feat: gzip, gunzip file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
+const sourceFilePath = join(import.meta.dirname, 'files/archive.gz')
+const targetFilePath = join(import.meta.dirname, 'files/fileToCompress.txt')
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(sourceFilePath);
+    const writeStream = createWriteStream(targetFilePath);
+    const gunZipStream = createGunzip();
+
+    readStream.on('data', (chunk) => {
+        gunZipStream.write(chunk)
+    })
+
+    gunZipStream.on('data', (chunk) => {
+        writeStream.write(chunk)
+    })
+
+    readStream.on('end', () => {
+        gunZipStream.end();
+    })
+
+    gunZipStream.on('end', () => {
+        writeStream.end();
+    })
 };
 
 await decompress();
