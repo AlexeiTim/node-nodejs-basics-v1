fcdef47ac3a101b8bd6726d6bea6d53d41991229
timashkov <timashkov060896@gmail.com>
2025-04-25 21:12:38 +0300

    feat: parse args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = [];
+    for (let i = 0; i < args.length - 1; i += 2) {
+        const key = args[i];
+        const value = args[i + 1];
+        result.push(`${key.replace('--', '')} is ${value}`)
+    }
+    console.log(result.join(', '))
 };
 
 parseArgs();
