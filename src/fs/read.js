410e7704ee5635ddb367d0378c8735a2b7499430
timashkov <timashkov060896@gmail.com>
2025-04-25 20:37:24 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,34 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
+
+const TARGET_DIR = 'files'
+const TARGET_FILE_NAME = 'fileToRead.txt';
+const ERROR_MESSAGES = {
+    FS_OPERATION_FAILED: 'FS operation failed',
+}
+
+const checkFileExists = async (filePath) => {
+    try {
+        await fs.access(filePath);
+        return true
+    } catch(e) {
+        return false
+    }
+}
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const targetFilePath = join(import.meta.dirname, TARGET_DIR, TARGET_FILE_NAME);
+        const isTargetFileExists = await checkFileExists(targetFilePath);
+        if (!isTargetFileExists)
+            throw new Error(ERROR_MESSAGES.FS_OPERATION_FAILED)
+
+        const data = await fs.readFile(targetFilePath, 'utf-8');
+        console.log(data)
+    } catch(e) {
+        throw e;
+    }
 };
 
 await read();
