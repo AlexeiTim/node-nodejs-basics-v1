d39e10d8e2d880d8f6ff3c7d1b59aee5ce7c24a8
timashkov <timashkov060896@gmail.com>
2025-04-25 22:28:25 +0300

    feat: read, tranform, write streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,4 +1,14 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+const TARGET_FILE =  join(import.meta.dirname, 'files/fileToRead.txt')
 const read = async () => {
+    const readStream = createReadStream(TARGET_FILE);
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk)
+    })
+    readStream.on('end', () => {
+        console.log('\nprocess end')
+    })
     // Write your code here 
 };
 
