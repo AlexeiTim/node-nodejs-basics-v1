12cfe46aa592244975c073ad514fb2cb42fe87cb
timashkov <timashkov060896@gmail.com>
2025-04-28 20:59:46 +0300

    feat: workers

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
-// n should be received from main thread
+import { parentPort } from 'worker_threads'
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', (data) => {
+        try {
+            const result = nthFibonacci(data)
+            parentPort.postMessage(result)
+            parentPort.close()
+        } catch(e) {
+           throw new Error(e)
+        }
+    })
 };
 
 sendResult();
