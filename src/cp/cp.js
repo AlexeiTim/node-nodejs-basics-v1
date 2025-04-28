dc54d1c37e4854f25ec4b91a45452a857ba7bec5
timashkov <timashkov060896@gmail.com>
2025-04-28 22:22:50 +0300

    refactor: rewrite to events

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -3,8 +3,18 @@ import { join } from 'path';
 const spawnChildProcess = async (args) => {
     const filePath = join(import.meta.dirname, 'files/script.js') 
     const childProc = fork(filePath, [...args], { silent: true })
-    process.stdin.pipe(childProc.stdin)
-    childProc.stdout.pipe(process.stdout)
+
+    process.stdin.on('data', (data) => {
+        childProc.stdin.write(data)
+    })
+
+    childProc.stdout.on('data', (data) => {
+        process.stdout.write(data)
+    })
+
+    childProc.on('exit', () => {
+        process.exit()
+    })
 };
 
 // Put your arguments in function call to test this functionality
